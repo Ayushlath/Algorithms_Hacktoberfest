d4715d9874fff7cebb57bc281e3a920238ab3777
SV0052 <suvigyvarshney0052@gmail.com>
2021-10-16 22:07:27 +0530

    third

---
+++ swap.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int swap(int x,int y){
+	int temp=x;
+	x=y;
+	y=temp;
+	
+	
+}
+
+
+
+int main(){
+int a,b;
+cin>>a>>b;
+
+swap(a,b);
+return 0;
+}
+
